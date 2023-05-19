23dedb09e35cc759d79cd0f094c3abb561a79f37
NamSonGC98 <namsongc98@gmail.com>
2023-05-19 18:15:26 +0700

    update

--- score-keeper/public/player.js
+++ score-keeper/public/player.js
@@ -1,5 +1,7 @@
 let playerInput = document.getElementById("input_user");
 
+let playerRender = document.getElementById("namePlayer");
+
 function handleCreate() {
   fetch("http://localhost:3000/api/v1/players", {
     method: "POST",
@@ -9,7 +11,31 @@ function handleCreate() {
     body: JSON.stringify({ namePlayer: playerInput.value }),
   })
     .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      window.location.href = "/player";
+    })
+    .then((data) => {
+      console.log(data);
+      window.location.href = "/player";
+    })
     .catch((err) => {
       alert(err);
     });
 }
+
+let renderPlayer = "";
+function render() {
+  fetch("http://localhost:3000/api/v1/players")
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((player) => {
+        renderPlayer += `<div class="input_player">${player.namePlayer}</div> `;
+      });
+      playerRender.innerHTML = renderPlayer;
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+render();
